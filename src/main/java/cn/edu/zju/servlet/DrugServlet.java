4a3e64350ffdefa45e16c41dd566159dbb2532ab
LiZhibo3029 <zhibo.23@intl.zju.edu.cn>
2025-03-04 14:04:37 +0800

    update

--- src/main/java/cn/edu/zju/servlet/DrugServlet.java
+++ src/main/java/cn/edu/zju/servlet/DrugServlet.java
@@ -1,29 +1,28 @@
 package cn.edu.zju.servlet;
 
+import cn.edu.zju.bean.Drug;
+import cn.edu.zju.dao.DrugDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
-@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
-public class CounterServlet extends HttpServlet {
-
-    private int counter = 0;
-
+@WebServlet(name = "DrugServlet",  urlPatterns = "/drugs")
+public class DrugServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        counter = counter + 1;
-        PrintWriter out = response.getWriter();
-        out.write("<html>");
-        out.write("<body>");
-        out.write("Total visit count: " + counter);
-        out.write("</body>");
-        out.write("</html>");
+
+        DrugDao drugDao = new DrugDao();
+        List<Drug> all = drugDao.findAll();
+
+        request.setAttribute("drugs", all);
+        request.getRequestDispatcher("/views/drugs.jsp").forward(request, response);
     }
 }
